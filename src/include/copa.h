795a4db9403408be031bc7cbf32b828b87c4d75b
pentsinh <2232962228@qq.com>
2025-04-21 23:06:53 +0800

    Add files via upload

---
+++ src/include/copa.h
@@ -0,0 +1,8 @@
+#ifndef _COPA_H_
+#define _COPA_H_
+
+int c_p(char *source_path, char *dest_father_path);
+int copy_file(const char *source_path, const char *dest_path);
+int rm_file(char *file_path);
+int rm_dir(char *dir_path);
+#endif
--- src/include/srch.h
+++ src/include/srch.h
@@ -3,10 +3,10 @@
 
 /************************************************
  *FUNCTION:搜索模式下，从键盘读取输入
- *INPUT:搜索目标，用于存放搜索结果的info
+ *INPUT:搜索目标，用于存放搜索结果的info，用于指向被loading遮挡图像的指针
  *RETURN:返回1：有输入；返回0：无输入
  *************************************************/
-int srch_input(char target[16], struct file_info *info);
+int srch_input(char target[16], struct file_info *info, void *buffer);
 
 /************************************************
  *FUNCTION:启动搜索！！！
