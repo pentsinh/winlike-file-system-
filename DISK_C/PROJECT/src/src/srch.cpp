6be74efc108d2e966bbefa6f78d40aac47f8c760
pentsinh <2232962228@qq.com>
2025-05-14 23:46:41 +0800

    搜索弃用目录流方法

--- DISK_C/PROJECT/src/src/srch.cpp
+++ DISK_C/PROJECT/src/src/srch.cpp
@@ -10,8 +10,8 @@ int srch_input(char target[16], struct file_info *info, void *buffer)
         int position = 0;
         info_init(info);
         loading(buffer);
-        srch("C:\\PROJECT", target, &position, info, 0); // 虚拟环境只能进行小范围搜索，大范围搜索会出现遗漏甚至找不到的情况
-        // srch("C:\\", target, &position, info, 0); // 上机改为这个
+        //srch("C:\\PROJECT", target, &position, info, 0); // 虚拟环境只能进行小范围搜索，大范围搜索会出现遗漏甚至找不到的情况
+        srch("C:\\", target, &position, info, 0); // 上机改为这个
 
         return 2;
     }
@@ -81,6 +81,7 @@ int srch_input(char target[16], struct file_info *info, void *buffer)
 //     closedir(dir);
 // }
 
+/*
 // 启动搜索！！！参数：搜索路径，目标，将要存入info的位置，用于存放信息的info数组,递归深度
 void srch(char *path, char *target, int *position, struct file_info *info, int depth)
 {
@@ -139,9 +140,10 @@ void srch(char *path, char *target, int *position, struct file_info *info, int d
 
     closedir(dir);
 }
+*/
 
 // 搜索  备用
-/*void srch(char *path, char *target, int *position, struct file_info *info, int depth)
+void srch(char *path, char *target, int *position, struct file_info *info, int depth)
 {
     struct ffblk ff;
     // printf("entering->%s ", path);
@@ -152,8 +154,7 @@ void srch(char *path, char *target, int *position, struct file_info *info, int d
     // printf("name=%s ", ff.ff_name);
     while (findnext(&ff) == 0)
     {
-        // printf("name=%s ", ff.ff_name);
-        char *file_path = get_file_path(path, ff.ff_name);
+        // printf("name=%s ", ff.ff_name);      
         if ((ff.ff_attrib & FA_ARCH) && strcmp(ff.ff_name, "..") != 0 && strcmp(ff.ff_name, ".") != 0) // 文件
         {
             if (strcmp(ff.ff_name, target) == 0 && (*position) < 10)
@@ -164,6 +165,7 @@ void srch(char *path, char *target, int *position, struct file_info *info, int d
         }
         else if ((ff.ff_attrib & FA_DIREC) && strcmp(ff.ff_name, "..") != 0 && strcmp(ff.ff_name, ".") != 0) // 文件夹
         {
+            char *file_path = get_file_path(path, ff.ff_name);
             if (strcmp(ff.ff_name, target) == 0 && (*position) < 10)
             {
                 // printf("find %s\n", path);
@@ -172,10 +174,11 @@ void srch(char *path, char *target, int *position, struct file_info *info, int d
             }
             if (depth < 7) // 限制递归深度
                 srch(file_path, target, position, info, depth + 1);
+            free(file_path);
         }
     }
     chdir("..");
-}*/
+}
 
 void srch_output(char path[1024], struct file_info *info, struct My_filenode *root, char *target)
 {
